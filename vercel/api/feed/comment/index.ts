2b2b81cd019d1a3bca93c775f548c7ea02154df1
rinorocks8 <rinorocks@gmail.com>
2023-06-06 20:39:06 -0400

    comment notification

--- vercel/api/feed/comment/index.ts
+++ vercel/api/feed/comment/index.ts
@@ -1,12 +1,14 @@
 import { dynamoDBRequest } from "../../utils/dynamoDBRequest";
 import { verifyCognitoToken } from "../../utils/verifyCognitoToken";
-import { BodyError } from "../../utils/errors";
+import { AuthenticationError, BodyError, ParameterError } from "../../utils/errors";
 import * as responder from '../../utils/responder';
-import { Comment, WorkoutKey } from '../../graphql'
+import { Comment, FollowingKey, User, UserKey, Workout, WorkoutKey } from '../../graphql'
 
 import { z } from "zod";
 import { AttributeValue } from 'dynamodb-data-types';
 import { moderationRequest } from "../../utils/moderationRequest";
+import { cognitoRequest } from "../../utils/cognitoRequest";
+import { deliverNotification } from "../../utils/deliverNotification";
 
 export const config = {
   runtime: "experimental-edge",
@@ -29,8 +31,31 @@ export default async function handleRequest(req: Request): Promise<Response> {
       throw new BodyError("Inappropriate Comment Content");
     }
 
-    const workout: WorkoutKey = {
-      workoutID: body.workoutID,
+    //Pull up workout -> get userID
+    const workoutKey: WorkoutKey = {
+      workoutID: body.workoutID
+    }
+    const getWorkout = {
+      TableName: process.env['Workout'],
+      Key: AttributeValue.wrap(workoutKey)
+    };
+    let _workout = await dynamoDBRequest("GetItem", getWorkout);
+    const workout: Workout = AttributeValue.unwrap(_workout.Item)
+
+    //check up following
+    if (username !== workout.userID) {
+      const followingKey: FollowingKey = {
+        userID: username,
+        followingUserID: workout.userID,
+      };
+      const getFollowing = {
+        TableName: process.env['Following'],
+        Key: AttributeValue.wrap(followingKey)
+      };
+      const following = await dynamoDBRequest("GetItem", getFollowing)
+      if (following.Item === undefined) {
+        throw new AuthenticationError("Not Following User")
+      }
     }
 
     const comment: Comment = {
@@ -46,7 +71,7 @@ export default async function handleRequest(req: Request): Promise<Response> {
         {
           Update: {
             TableName: process.env["Workout"],
-            Key: AttributeValue.wrap(workout),
+            Key: AttributeValue.wrap(workoutKey),
             UpdateExpression: "SET #comments = if_not_exists(#comments, :default) + :incr",
             ConditionExpression: "attribute_exists(userID) and attribute_exists(workoutID)",
             ExpressionAttributeNames: { "#comments": "comments" },
@@ -70,6 +95,32 @@ export default async function handleRequest(req: Request): Promise<Response> {
         throw new BodyError("Workout Not Found");
       throw error;
     })
+
+    //Get username and deviceId
+    const userKey: UserKey = {
+      userID: workout.userID
+    }
+    const getUser = {
+      TableName: process.env['User'],
+      Key: AttributeValue.wrap(userKey)
+    };
+    const [_user, userCognito] = await Promise.all([
+      dynamoDBRequest("GetItem", getUser),
+      cognitoRequest(	
+        "AdminGetUser", {
+          Username: username,
+          UserPoolId: process.env.userPoolID,
+        }).catch((error) => {
+          if (error.message === "User does not exist.")
+            throw new ParameterError("User Not Found");
+        })
+    ])
+    const preferred_username = userCognito.UserAttributes?.find(
+      (obj) => obj.Name === "preferred_username"
+    )?.Value;
+    const user: User = AttributeValue.unwrap(_user.Item)
+    if (user?.endpointArn)
+      deliverNotification(user?.endpointArn, `@${preferred_username} commented: "${body.text}"`)
     
     return responder.success({
       result: "Commented",
